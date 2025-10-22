1c430ceafb6ed4928a26f4afaf0be0d84d378da4
yuutsu <nurikbaidibek@gmail.com>
2025-10-22 21:28:17 +0500

    refactor(utils/activation_functions): new structure and optimizations

--- library/utils/activation_functions.py
+++ library/utils/activation_functions.py
@@ -1,19 +1,39 @@
 #import python modules
-import math
+from abc import ABC, abstractmethod
 #import package modules
 import numpy as np
 #import local modules
 #pass
+
+class ActivationFunction(ABC):
+    @abstractmethod
+    def activate(self, x):
+        pass
+    @abstractmethod
+    def derivative(self,x):
+        pass
+
+class Relu(ActivationFunction):
+    """Rectified Linear Unit"""
+    @staticmethod
+    def activate(x : np.ndarray) -> np.ndarray:
+        return np.maximum(0, x)
+    @staticmethod
+    def derivative(x : np.ndarray) -> np.ndarray:
+        return np.where(x > 0, 1, 0)
+
+class Sigmoid(ActivationFunction):
+    """Sigmoid"""
+    @staticmethod
+    def activate(x : np.ndarray) -> np.ndarray:
+        x_safe = np.clip(x, -500, 500)
+        return 1 / (1 + np.exp(-x_safe))
+    @staticmethod
+    def derivative(x : np.ndarray) -> np.ndarray:
+        s = Sigmoid.activate(x)
+        return s * (1 - s)
+
+
 class ActivationFunctions:
-    @staticmethod
-    def relu(x):
-        """Rectified linear unit"""
-        return max(0,x)
-    @staticmethod
-    def sigmoid(x):
-        """Sigmoid"""
-        return 1 / (1 + math.exp(-x))
-    @staticmethod
-    def tanh(x):
-        """Hyperbolic Tangent"""
-        return math.tanh(x)
+    RELU = Relu
+    SIGMOID = Sigmoid
