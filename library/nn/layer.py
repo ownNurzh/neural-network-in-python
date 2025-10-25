c4ed545513a59f129b05f243bde11fbb172b955f
yuutsu <nurikbaidibek@gmail.com>
2025-10-25 11:13:26 +0500

    feat(nn) : finished init params in layer,added some type hints

--- library/nn/layer.py
+++ library/nn/layer.py
@@ -7,14 +7,15 @@ import numpy as np
 from library.utils import TYPE
 #end
 class Layer:
-    _units : int
+    _units : int 
     _activation_func : TYPE.ACTIVATION_FUNCTION
-    _weights : np.ndarray
-    _biases : np.ndarray
+    _weights : np.ndarray | None = None
+    _biases : np.ndarray | None = None
     def __init__(self,units:int,activation_func:TYPE.ACTIVATION_FUNCTION) -> None:
         self._units = units
         self._activation_func = activation_func
+        self.init_params()
     def init_params(self) -> None:
-        self._weights = None
-        self._biases = None
+        self._weights = np.random.rand(self._units)
+        self._biases = np.zeros(self._units)  
     
