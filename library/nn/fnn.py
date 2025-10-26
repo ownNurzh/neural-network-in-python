146262626fa579461e78abc072e2c1379f449a78
yuutsu <nurikbaidibek@gmail.com>
2025-10-26 18:58:11 +0500

    test(tests): added new tests in fnn initialization feat(fnn): added Weight initialization in FNN class

--- library/nn/fnn.py
+++ library/nn/fnn.py
@@ -4,11 +4,14 @@
 #import package modules
 import numpy as np
 #import local modules
-from .layer import Layer
+from library.nn import Layer
+from library.nn import Weight
 #end
 class FNN :
     _layers:list[Layer] = []
+    _weight:Weight
     def __init__(self,layers:list[Layer]) -> None:
         self._layers = layers
+        self._weight = Weight(layers)
     def forward(self,inp : np.ndarray) -> np.ndarray:
         pass
--- tests/test_fnn.py
+++ tests/test_fnn.py
@@ -7,7 +7,9 @@ import numpy as np
 #import local modules
 from library.nn import FNN
 from library.nn import Layer
+from library.nn import Weight
 from library.utils import ActivationFunctions
+
 #end
 
 def test_fnn_initialization():
@@ -15,7 +17,8 @@ def test_fnn_initialization():
         Layer(3, ActivationFunctions.RELU),
         Layer(2, ActivationFunctions.SIGMOID)
     ]
-    
+    new_weight = Weight(layers=structure)
     fnn = FNN(layers=structure)
     assert fnn._layers == structure
+    assert len(fnn._weight._weights) == len(new_weight._weights)
     
