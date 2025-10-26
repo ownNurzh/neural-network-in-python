24555959ca34086f9e4429de53d018b1a90d2b6a
yuutsu <nurikbaidibek@gmail.com>
2025-10-26 18:14:11 +0500

    refactor(nn): deleted weights in Layer

--- library/nn/layer.py
+++ library/nn/layer.py
@@ -9,13 +9,11 @@ from library.utils import TYPE
 class Layer:
     _units : int 
     _activation_func : TYPE.ACTIVATION_FUNCTION
-    _weights : np.ndarray | None = None
     _biases : np.ndarray | None = None
     def __init__(self,units:int,activation_func:TYPE.ACTIVATION_FUNCTION) -> None:
         self._units = units
         self._activation_func = activation_func
         self.init_params()
     def init_params(self) -> None:
-        self._weights = np.random.rand(self._units)
         self._biases = np.zeros(self._units)  
     
