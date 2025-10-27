4998d227d1ba4ebd1a4475252381fb2273b3c7ab
yuutsu <nurikbaidibek@gmail.com>
2025-10-28 00:04:30 +0500

    feat(main) : add normalization iris data set

--- main.py
+++ main.py
@@ -7,13 +7,37 @@ import numpy as np
 from library.nn import Layer
 from library.nn import FNN
 from library.utils import ActivationFunctions
+from library.utils import z_score_normalization
 #end
 
 #Import iris data set
 print("Importing data set...")
-with open("iris_dataset.json", "r", encoding="utf-8") as f:
-    
+with open("iris_dataset.json", "r", encoding="utf-8") as f: 
     data = json.load(f)
-    data = np.array(data)
-    print(f"File data shape: {data.shape}")
+    print(f"File data len: {len(data)}")
     print(f"First index data: {data[0]}")
+
+#Normalization data set
+print("Normalization...")
+datas_for_normalization = ["sepal_length","sepal_width","petal_length","petal_width"]
+print("Param for normalization: ",datas_for_normalization)
+for d in datas_for_normalization:
+    print(f"Normalizing {d} ...")
+    values = np.array([float(el[d]) for el in data])
+    z_scores = z_score_normalization(values)
+    print(f"Before data sum : {np.sum(values)}")
+    print(f"After data sum : {np.sum(z_scores)}")
+    #update values
+    for i in range(len(data)):
+        data[i][d] = z_scores[i]
+    a = data[0][d]
+    print(f"Updated values {d},finish test type - {type(a)}, value - {a} ")
+
+
+#Create nn
+structure = [
+    Layer(4, ActivationFunctions.RELU),
+    Layer(12, ActivationFunctions.RELU),
+    Layer(3, ActivationFunctions.SOFTMAX)
+]
+yuutsu = None
